efe840e3197e16f066f59ec8df6a142eb8f84626
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-04-10 10:14:50 +0200

    Refined header component

--- src/app/features/common/header/header.component.ts
+++ src/app/features/common/header/header.component.ts
@@ -8,4 +8,20 @@ import {HeaderButtonsConstants} from "../../../constants/header-buttons.constant
 })
 export class HeaderComponent {
   public buttons = HeaderButtonsConstants.buttons
+  public active = "nav__menu";
+  public icon = "menu";
+
+  public navToggle = () => {
+    if (this.active === 'nav__menu') {
+      this.active = "nav__menu nav__active";
+    } else {
+      this.active = "nav__menu";
+    }
+
+    if (this.icon === "menu") {
+      this.icon = "close";
+    } else {
+      this.icon = "menu";
+    }
+  }
 }
