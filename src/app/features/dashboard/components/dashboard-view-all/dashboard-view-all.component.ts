6fa3540a2d87712d00c73884f917523b88e3abef
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-06-05 19:30:56 +0200

    Removed periodic element interface

--- src/app/features/dashboard/components/dashboard-view-all/dashboard-view-all.component.ts
+++ src/app/features/dashboard/components/dashboard-view-all/dashboard-view-all.component.ts
@@ -1,12 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
 @Component({
   selector: 'app-dashboard-view-all',
   templateUrl: './dashboard-view-all.component.html',
